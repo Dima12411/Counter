b3ce0f5547903a133ebe8429dcfea0600baffb17
Dima12411 <fixer40delta@gmail.com>
2021-12-12 21:28:28 +0300

    Properties fot the counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./Counter";
+import PropertiesForTheCounter from "./PropertiesForTheCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number, setNumber] = useState<number>(0)
+    const [max, setMax] = useState<number>(5)
+    const [start,setStart] = useState<number>(0)
+    const startValue = start
+    const addNumber = () => {
+        if (start < max) {
+            setNumber(number + 1)
+        }
+    }
+    const resetNumber = () => {
+        setNumber(start)
+    }
+    const onChangeMax = (e: ChangeEvent<HTMLInputElement>) => {
+        setMax(e.currentTarget.valueAsNumber)
+    }
+    const onChangeStart = (e: ChangeEvent<HTMLInputElement>) => {
+        setStart(e.currentTarget.valueAsNumber)
+    }
+    const messageOutput = () => {
+
+    }
+    return (
+        <div className="App">
+            <div className="body">
+                <Counter
+                addNumber={addNumber}
+                number={number}
+                resetNumber={resetNumber}
+                start={start}
+                max={max}
+            />
+            </div>
+            <div className="body2">
+                <PropertiesForTheCounter
+                max={max}
+                setMax={setMax}
+                start={start}
+                setStart={setStart}
+                onChangeMax={onChangeMax}
+                onChangeStart={onChangeStart}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
